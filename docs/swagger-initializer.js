1a2117a856334bb6a247031c1ba33e6b9a6ec074
seongchanhwang12 <tjdcksghkd12@gmail.com>
2025-11-17 00:42:59 +0900

    [INIT] 서버 설계서 init - api-spec - infra 구성도 - ERD 구성도 - 주요 흐름 sequence diagram 작성

---
+++ docs/swagger-initializer.js
@@ -0,0 +1,6 @@
+window.ui = SwaggerUIBundle({
+    url: "/openapi.yaml",
+    dom_id: '#swagger-ui',
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    layout: "StandaloneLayout"
+});
